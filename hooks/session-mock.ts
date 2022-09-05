ab4b77b7176b6f5ba335c04be6e064268494a98b
Viktor Nagy <viktor.nagy@01people.com>
2022-09-05 15:36:05 +0200

    refactor: properly type around sessionMock

--- hooks/session-mock.ts
+++ hooks/session-mock.ts
@@ -1,7 +1,7 @@
-import { useSession } from "next-auth/react";
+import { SessionContextValue, useSession } from "next-auth/react";
 
 const isMockLoggedIn = true;
-export function useMockSession() {
+export function useMockSession(): SessionContextValue {
   if (process.env.NEXT_PUBLIC_ENV === "production") return useSession();
   else {
     return isMockLoggedIn
@@ -12,11 +12,13 @@ export function useMockSession() {
               email: "test@test.sk",
               image: "https://avatars.githubusercontent.com/u/11056512?v=4",
             },
+            expires: "never",
           },
           status: "authenticated",
         }
       : {
           data: null,
+          status: "unauthenticated",
         };
   }
 }
